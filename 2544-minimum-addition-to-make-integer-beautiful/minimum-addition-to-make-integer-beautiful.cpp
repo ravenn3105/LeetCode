523da569b311992fd9cbe31ca4b1024e0a6d9a3d
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-01-30 22:07:54 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (8.74%) - LeetSync

--- 2544-minimum-addition-to-make-integer-beautiful/minimum-addition-to-make-integer-beautiful.cpp
+++ 2544-minimum-addition-to-make-integer-beautiful/minimum-addition-to-make-integer-beautiful.cpp
@@ -1,30 +1,20 @@
 class Solution {
-public:
-    long long makeIntegerBeautiful(long long int n, int target) {
-       int sum=  SumofDigits(n);
-       if (sum<= target){ return 0;}
-       long long int result=0;
-       long long int place=1;
-       while( sum>target){
-           long long int digit= n%10;
-           if (digit==0){
-               n/=10;
-               place*=10;
-               continue;
-           }
-           long long int diff= 10-digit;
-           result+= diff*place;
-           n =(n/10)+1;
-           place *=10;
-           sum= SumofDigits(n);
-       }
-       return result;
+    int sum(long long n) {
+        long long s = 0;
+        while(n) s += n % 10, n /= 10;
+        return s;
     }
-    int SumofDigits(long long n){
-        int sum=0;
-        while (n!=0){
-            sum+= n%10;
-            n /=10;
-        }return sum;
+public:
+    long long makeIntegerBeautiful(long long n, int target) {
+        long long org = n;
+        if(sum(n) <= target) return 0;
+        
+        long long r = 10;
+        while(sum(n) > target) {
+            if(n % r == 0) r *= 10;
+            n = (n / r + 1) * r;
+            r *= 10;
+        }
+        return n - org;
     }
 };

5067ea66c5fe574f26649df291e7069967a95589
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-02-02 23:33:06 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (9.62%) - LeetSync

--- 1212-sequential-digits/sequential-digits.cpp
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -6,15 +6,12 @@ public:
         for (int i=1; i<10; i++){
             int x=0;
             for (int j=i; j<10; j++){
-                if (x>high){
-                    break;
-                }else{
-                    x= x*10 +j;
-                    if (x>= low && x<= high){
-                        ans.push_back(x);
-                    }
+                x= x*10 +j;
+                if (x>= low && x<= high){
+                    
+                    ans.push_back(x);
                 }
-            }
+                }
         }
         sort(ans.begin(), ans.end());
         return ans;

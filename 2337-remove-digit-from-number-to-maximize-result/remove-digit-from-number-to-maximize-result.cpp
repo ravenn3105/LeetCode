3fd756cd8f773ca284d807ef400f29c53d973f8d
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-02-04 21:20:27 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (8.66%) - LeetSync

--- 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
+++ 2337-remove-digit-from-number-to-maximize-result/remove-digit-from-number-to-maximize-result.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    string removeDigit(string number, char digit) {
-        string res="0";
-        for(int i=0; i<number.size(); i++)
+    string removeDigit(string number, char digit) 
+    {
+        string ans = "";
+        for(int i = 0; i < number.size(); i++)
         {
-            if(number[i]==digit)
+            if(number[i] == digit)
             {
-                string left=number.substr(0, i);
-                string right=number.substr(i+1, number.size()-i-1);
-                string str=left+right;
-                if(str>res)
-                {
-                    res=str;
-                }
+                string help = number.substr(0,i) + number.substr(i+1, number.size());
+                ans = max(ans, help);
             }
         }
-        return res;
+        return ans;
     }
 };

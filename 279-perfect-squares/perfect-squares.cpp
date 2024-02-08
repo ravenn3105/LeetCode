6ed0a27d4f8b63b3d5d6d65302f323763695d3f2
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-02-08 18:30:16 +0530

    Time: 4 ms (98.08%) | Memory: 7.3 MB (90.77%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,16 +1,27 @@
+
+
 class Solution {
 public:
     int numSquares(int n) {
-      vector<int> dp(n+1);
-      dp[0]=0;  
-      dp[1]=1;  
-      for (int i=2; i<=n; i++){
-          int min= INT_MAX;
-          for (int j=1; j*j<=i; j++){
-              int r= i-j*j;
-              if( dp[r]< min){ min=dp[r];}
+        // Check if the number is not of the form 4^a(8b+7)
+        while (n % 4 == 0)//o(logN)
+            n /= 4;
+        if (n % 8 == 7)
+            return 4;
 
-          }dp[i]= min+1;
-      }return dp[n];
+        int sqrt_n = int(sqrt(n));//O(1) 
+        if (sqrt_n * sqrt_n == n)//O(1)
+            return 1;
+
+        for (int i = 1; i * i <= n; ++i) {
+            int j = int(sqrt(n - i * i));  //O(sqrt(n))
+            if (i * i + j * j == n)
+                return 2;
+        }
+
+//overall TC O(sqrt(n))
+//space Complexity O(1)
+        return 3;
     }
 };
+

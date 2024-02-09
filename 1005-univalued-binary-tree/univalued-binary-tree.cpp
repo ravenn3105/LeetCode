04161f7b355bfa336c5ab1b3d40ffbd35ff4ed4a
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-02-10 00:16:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (10.31%) - LeetSync

--- 1005-univalued-binary-tree/univalued-binary-tree.cpp
+++ 1005-univalued-binary-tree/univalued-binary-tree.cpp
@@ -1,24 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    void traverse(TreeNode* node, const int& value, bool& is_univalued)
-    {
-        if (node == nullptr || !is_univalued)
-            return;
-        if (node->val != value)
-        {
-            is_univalued = false;
-            return;
-        }
-        traverse(node->left, value, is_univalued);
-        traverse(node->right, value, is_univalued);
-    }
 public:
-    bool isUnivalTree(TreeNode* root) {
-        if (root == nullptr)
+    bool traversal( TreeNode * root, int value){
+        if (root==NULL){return true ;}
+        if ((root->val!=value)){
             return false;
-        const int value = root->val;
-        bool is_univalued = true;
-        traverse(root, value, is_univalued);
-        return is_univalued;
+        }
+        return (traversal(root->left, value) && traversal(root->right, value));
     }
+    bool isUnivalTree(TreeNode* root) {
+        
+        if(root == nullptr) return true;
+        const int value= root->val;
+        return traversal(root, value);
+        
+    }
+
 };

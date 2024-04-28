21a94cde995831f021b30d0bc1a2d810075be2f4
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-04-28 22:09:37 +0530

    Time: 327 ms (22.47%) | Memory: 150.3 MB (5.18%) - LeetSync

---
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+vector<int> ans, count, subTree;
+vector<unordered_set<int>> graph;
+int N;
+void dfs( int node, int parent){
+    for(int c: graph[node]){
+        if (c!= parent){
+            dfs(c,node);
+            count[node]+= count[c];
+            subTree[node]+= subTree[c] +count[c];
+        }
+    }
+    count[node]++;
+}
+void dfs2(int node, int parent){
+    for (auto c:graph[node]){
+        
+        if(c!=parent){
+            ans[c]=ans[node]-count[c]+N- count[c];
+            dfs2(c,node);
+        }
+    }
+}
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        N = n;
+        graph.resize(N);
+        ans.resize(N);
+        count.resize(N);
+        subTree.resize(N);
+        for (int i=0; i<n; ++i){
+            graph[i]= (std::unordered_set<int>());
+        }
+        for (auto edge:edges){
+            graph[edge[0]].insert(edge[1]);
+            graph[edge[1]].insert(edge[0]);
+        }
+        dfs(0,-1);
+        ans[0]= subTree[0];
+        dfs2(0,-1);
+        return ans;
+    }
+};

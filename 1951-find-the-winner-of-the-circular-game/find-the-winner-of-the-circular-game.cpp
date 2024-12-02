c1c71ca359709de9360b493e19928323f9acce3d
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-12-02 22:34:31 +0530

    Time: 13 ms (38.74%) | Memory: 8.2 MB (36.61%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,11 +1,27 @@
 class Solution {
 public:
-
-    int winner(int n,int k){
-        if(n==1)return 0;
-        return (winner(n-1,k)+k)%n;
-    }
+int factor(int n, int k){
+    int y;
+    // Find the largest x such that k^x <= n
+    if (k!=1){int x = log(n) / log(k);
+    y = n - pow(k, x);}
+    else {return n;}
+    // Compute y
+    
+    
+    return y;
+}
     int findTheWinner(int n, int k) {
-        return winner(n,k)+1;
+        int p, i=0;
+        p= factor(n,k);
+        int winner = 1;
+        
+        while(i<=p){
+        for(int groupSize = 1; groupSize <= n; groupSize++){
+            winner = ((winner + k - 1) % groupSize) + 1;
+
+        }
+        i++;}
+        return winner;
     }
 };

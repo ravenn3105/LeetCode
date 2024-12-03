6009a09a648aa08d85809fa5b19236d0b3416313
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2024-12-03 10:57:12 +0530

    Time: 10 ms (40.63%) | Memory: 8.1 MB (37.74%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-int factor(int n, int k){
+int factor(int n, int k){      //o(n)=1
     int y;
-    // Find the largest x such that k^x <= n
-    if (k!=1){int x = log(n) / log(k);
-    y = n - pow(k, x);}
+    // Find y such that n= k^x +y 
+    if (k!=1){
+        int x = log(n) / log(k);
+        y = n - pow(k, x);
+        }
     else {return n;}
-    // Compute y
-    
-    
     return y;
 }
     int findTheWinner(int n, int k) {
-        int p, i=0;
-        p= factor(n,k);
+        int y, i=0;
+        y= factor(n,k);
         int winner = 1;
-        
-        while(i<=p){
-        for(int groupSize = 1; groupSize <= n; groupSize++){
-            winner = ((winner + k - 1) % groupSize) + 1;
-
+        //runs till y number of steps are completed
+        while(i<=y){        //O(y+1)
+        for(int groupSize = 1; groupSize <= n; groupSize++){    //O(n)
+            winner = ((winner + k - 1) % groupSize)+1 ;
+            }
+            i++;
         }
-        i++;}
         return winner;
-    }
+    }   //overall time complexity= O(n^2)
+        //space complexity= O(1)
 };

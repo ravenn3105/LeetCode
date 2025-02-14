3f8b286611ac0a226734c53b99867bce14c309c7
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2025-02-15 00:27:30 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (95.06%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
+void dfs(vector<int>& nums, vector<vector<int>> &ans, vector<int> &subset, int i){
+    if (i>= nums.size()){
+        ans.push_back(subset);
+        return ;
+    }
+    subset.push_back(nums[i]);
+    dfs(nums, ans,subset, i+1);
+    subset.pop_back();
+    dfs(nums, ans,subset, i+1);
+}
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans= {{}}; 
-        for (auto x: nums) {
-            int size=ans.size();
-            for (int i=0;i<size; i++) {
-                vector<int> subset = ans[i];
-                subset.push_back(x);
-                ans.push_back(subset);
-            }
-        }
+        vector<vector<int>> ans;
+        vector<int> subset;
+        dfs(nums,ans,subset,0);
         return ans;
     }
 };

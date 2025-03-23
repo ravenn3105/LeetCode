e5a04f9687b4bb0c09adb24a9a52bc1ec309d196
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2025-03-24 00:28:55 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (95.85%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -8,27 +8,37 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     void reorderList(ListNode* head) {
-         vector<int> v1;
-        ListNode* temp2 = head;
-        while(temp2 != NULL){
-            v1.push_back(temp2->val);
-            temp2 = temp2->next;
+        ListNode* slow= head;
+        ListNode* fast= head->next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast= fast->next->next;
         }
-        
-        int i = 0, j = v1.size()-1;
-        while(i <= j || head != NULL){
-            head->val = v1[i];
-            head = head->next;
-            if(head != NULL) 
-            {
-              head->val = v1[j];
-              head = head->next;
-            }
-            i++;
-            j--;
+
+        ListNode* curr= slow->next;
+        ListNode* prev= slow->next=NULL;
+        while(curr!=NULL){
+            ListNode* t= curr->next;
+            curr->next= prev;
+            prev= curr;
+            curr= t;
         }
+
+        ListNode* a= head;
+        curr= prev;
+        while(curr!= NULL){
+            ListNode* temp1= a->next;
+            ListNode* temp2= curr->next;
+
+            a->next= curr;
+            curr->next= temp1;
+            a=temp1;
+            curr= temp2;
+        }
+
     }
 };

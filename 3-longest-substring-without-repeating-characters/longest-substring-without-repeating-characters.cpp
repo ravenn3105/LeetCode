761a53464ae024a6b1003b322e4653958d8c28e1
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2025-06-12 00:04:34 +0530

    Time: 23 ms (20.03%) | Memory: 16.9 MB (12.29%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n= s.size();
+        if (n==0) return 0;
+        set<char> charSet;
+    
+        int i=0, maxLen=0;
+        for (int j=i; j<n; j++){
+            while (charSet.find(s[j]) != charSet.end()){
+                charSet.erase(s[i]);
+                i++;
+
+            }
+            charSet.insert(s[j]);
+            maxLen= max(maxLen, (j-i+1));
+        }
+        return maxLen;
+    }
+};

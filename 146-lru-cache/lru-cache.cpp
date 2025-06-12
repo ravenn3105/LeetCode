868f115e146e952e974d5575f7bfc3e674619746
Riya Singh <88481762+ravenn3105@users.noreply.github.com>
2025-06-12 23:16:30 +0530

    Time: 64 ms (83.13%) | Memory: 178 MB (52.79%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,71 @@
+class Node{
+    public:
+    int key,val;
+    Node * prev;
+    Node * next;
+    Node(int k, int v):key(k), val(v), prev(nullptr), next(nullptr){}
+
+};
+
+
+
+class LRUCache {
+private:
+int cap;
+    unordered_map<int, Node*> cache;
+    Node* left;
+    Node* right;
+
+    void remove(Node* node){
+        Node* pre= node->prev;
+        Node* nxt= node->next;
+        pre->next= nxt;
+        nxt->prev=pre;
+    }
+
+    void insert(Node* node){
+        Node* pre= right->prev;
+        pre->next= node;
+        node->prev= pre;
+        node->next= right;
+        right->prev= node;
+
+    }
+public:
+    
+    LRUCache(int capacity) {
+        cap =capacity;
+        cache.clear();
+        left= new Node(0,0);
+        right= new Node(0,0);
+        left->next=right;
+        right->prev=left;
+
+    }
+    
+    int get(int key) {
+        if (cache.find(key)!= cache.end()){
+            Node* node= cache[key];
+            remove(node);
+            insert(node);
+            return node->val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if (cache.find(key)!= cache.end()){
+            remove(cache[key]);
+        }
+        Node* newnode= new Node(key,value);
+        cache[key]=newnode;
+        insert(newnode);
+
+        if(cache.size()>cap){
+            Node* lru= left->next;
+            remove(lru);
+            cache.erase(lru->key);
+            delete lru;
+        }
+    }
+};
